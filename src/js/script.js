e6b9033c12fa7db41dc5b033e11ecf818e4e4230
Gustavo Sales <gustavo@useorigin.com>
2021-01-24 12:06:13 -0300

    add event track correctly

--- src/js/script.js
+++ src/js/script.js
@@ -828,10 +828,8 @@
 	$('[data-gift-list-name]').on('click', function() {
 		const listName = $(this).data('gift-list-name');
 
-		gtag('event', listName + '_click', {
-			send_to: 'G-LP423FLDNS',
-			event_category: 'Gift List',
-			event_label: 'Wedding'
+		gtag('event', 'page_view', {
+			page_title: listName
 		});
 	});
 })(window.jQuery);
